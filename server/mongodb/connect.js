eb9886980deb0b17d9a1b2a9d4871b572bd29494
Shep221 <Lucas02.song@gmail.com>
2023-02-18 23:45:03 -0800

    full code

---
+++ server/mongodb/connect.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const connectDB = (url) => {
+  mongoose.set('strict', true);
+  mongoose.connect(url)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log('Error connecting to MongoDB', err));
+}
+
+export default connectDB;
